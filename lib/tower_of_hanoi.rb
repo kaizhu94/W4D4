1f111e2cff2b6d91ff220885705577a5236c4cd7
Paul Ramirez <pramirez7230@bths.edu>
2020-11-19 16:01:49 -0500

    Switch driver and navigator

--- lib/tower_of_hanoi.rb
+++ lib/tower_of_hanoi.rb
@@ -1,11 +1,17 @@
 
 class TowerOfHanoi
     attr_reader :towers
-    def initialize(towers)
-        @towers = towers
+    def initialize
+        @towers = [[4, 3, 2, 1],[],[]]
     end
 
     def move(start_tower, target_tower)
+      # @towers[target_tower] << @towers[start_tower].pop
+    end
+
+    def valid_move?(start_tower, target_tower)
 
     end
+    # private
+
 end
--- spec/tower_of_hanoi_spec.rb
+++ spec/tower_of_hanoi_spec.rb
@@ -2,30 +2,38 @@ require 'tower_of_hanoi.rb'
 
 describe TowerOfHanoi do 
 
-    subject(:game){TowerOfHanoi.new([[4,3],[2,1],[]])}
+    subject(:game) {TowerOfHanoi.new}
 
     describe "#initialize" do
-        # it "sets towers" do 
-        #     expect(game.towers).to eq([[4,3],[2,1],[]])
-        # end
-        it "takes one argument: towers" do
-            expect { game }.not_to raise_error
-        end
-        context "with valid argument" do 
-            it "sets towers" do 
-            expect(game.towers).to eq([[4,3],[2,1],[]])
-            end
+        it "sets towers" do 
+            expect(game.towers).to eq([[4, 3, 2, 1],[],[]])
         end
     end
 
-    describe "#move" do 
-        context
+	describe "#move" do 
+		context "takes two arguments; start_tower and target_tower" do
+			it
+
+		end
+
         context "when moving a disc" do 
             it "move the top disc from one tower to another tower" do 
-                expect(move(1,2)).to eq([[4,3],[2],[1]])
-            end
+                expect(game.move(,2)).to eq([[4,3],[2],[1]])
+			end
+			
+			i
         end
-    end
+	end
+	
+	describe "#valid_move?" do
+		it "should start from a valid tower" do 
+			
+			expect(game[1])).to_not be_empty
+		end
+
+
+	end
+
 
     describe "#won?" do 
         it "checks if a tower is fully stacked and others are empty" do 
